69f0de6cb407495d2f1fae9e608f65f1e7f5d152
rabeeh003 <rabimon003@gmail.com>
2024-12-23 17:49:07 +0530

    fix redux persisit issue

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
+import { combineReducers } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // default localStorage
+import storage from 'redux-persist/lib/storage'; // Default localStorage
+
 import menuReducer from './menuSlice';
 import cartReducer from './cartSlice';
 import appReducer from './appSlice';
@@ -9,18 +11,23 @@ import appReducer from './appSlice';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['cart', 'app'], // specify which reducers to persist
+  whitelist: ['cart', 'app'], // Specify which reducers to persist
+  debug: true,
 };
 
-// Wrap reducers with persistReducer
-const rootReducer = {
-  cart: persistReducer(persistConfig, cartReducer),
-  app: persistReducer(persistConfig, appReducer),
+// Combine reducers
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  app: appReducer,
   menu: menuReducer,
-};
+});
 
+// Wrap the rootReducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the store
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
 // Create the persistor
